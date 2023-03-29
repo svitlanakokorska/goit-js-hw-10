91e884f52bf18b775686cb3af00ff151c568f54e
svitlanakokorska <s.kokorskaya@gmail.com>
2023-03-29 20:12:05 +0300

    виправлення помилок

--- src/JS/fetchCountries.js
+++ src/JS/fetchCountries.js
@@ -1,19 +1,19 @@
 export const fetchCountries = name => {
-    return fetch(
-      `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
-    )
-      .then(response => {
-        if (!response.ok) {
-          if (response.status === 404) {
-            return [];
-          }
-  
-          throw new Error(response.status);
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
+  )
+  .then(response => {
+      if (!response.ok) {
+        if (response.status === 404) {
+          return [];
         }
-  
-        return response.json();
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
+
+        throw new Error(response.status);
+      }
+
+      return response.json();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
